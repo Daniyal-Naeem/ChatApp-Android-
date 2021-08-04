9ed6b6f6b469797bab15dc0ce349fdee15931e06
Daniyal Naeem <daniyalnaeem5@gmail.com>
2021-08-04 11:34:10 +0500

    added app screens some left

---
+++ src/screens/StartScreen.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Paragraph from '../components/Paragraph'
+
+export default function StartScreen({ navigation }) {
+  return (
+    <Background>
+      <Logo />
+      <Header>Start Screen</Header>
+      <Paragraph>
+        Welcome to the best Chat App.
+      </Paragraph>
+      <Button
+        mode="contained"
+        onPress={() => navigation.navigate('LoginScreen')}
+      >
+        Login
+      </Button>
+      <Button
+        mode="outlined"
+        onPress={() => navigation.navigate('RegisterScreen')}
+      >
+        Register
+      </Button>
+    </Background>
+  )
+}
