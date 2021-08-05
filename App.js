70645b00679d981170c1a3cae52835e7f50e525f
Daniyal Naeem <daniyalnaeem5@gmail.com>
2021-08-05 16:15:53 +0500

    updated app.js

--- App.js
+++ App.js
@@ -1,43 +1,19 @@
 import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import { Provider } from 'react-native-paper'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
 import { theme } from './src/core/theme'
-import {
-  StartScreen,
-  LoginScreen,
-  RegisterScreen,
-  ResetPasswordScreen,
-  Dashboard,
-} from './src/screens'
+//import AuthNavigation from './navigation/AuthNavigation';
+import HomeNavigation from './navigation/HomeNavigation';
 
-const Stack = createStackNavigator()
 
 const App = () => {
   useEffect(()=> {
     SplashScreen.hide()
   }, [])
   return (
-
     <Provider theme={theme}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="StartScreen"
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="StartScreen" component={StartScreen} />
-          <Stack.Screen name="LoginScreen" component={LoginScreen} />
-          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-          <Stack.Screen name="Dashboard" component={Dashboard} />
-          <Stack.Screen
-            name="ResetPasswordScreen"
-            component={ResetPasswordScreen}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+   {/* <AuthNavigation /> */}
+     <HomeNavigation/>
     </Provider>
   )
 }
